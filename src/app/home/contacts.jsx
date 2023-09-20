da524c719468acabe4538ae54fc8ffcb83f2a307
ccdu1010 <ccdu1010@gmail.com>
2023-09-20 10:52:37 +0100

    Update to contacts page

--- src/app/home/contacts.jsx
+++ src/app/home/contacts.jsx
@@ -1,5 +1,6 @@
+import { useState, useEffect } from "react";
 import { Redirect } from "expo-router";
-import { View, Text } from "react-native";
+import { ScrollView } from "react-native";
 import { auth } from "../../firebase";
 import AddContact from "../../components/contacts/AddContact"
 import ContactList from "../../components/contacts/ContactList";
@@ -9,10 +10,40 @@ export default function ContactsPage() {
     return <Redirect href="/login" />;
   }
 
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+        // call backend here to get contacts instead of this
+        const contactsData = [
+            {
+                id: 1,
+                name: "Mum",
+                telNo: "07777 777 777",
+                avatarUrl: "https://cdn4.iconfinder.com/data/icons/cool-avatars-2/190/00-08-512.png",
+                defaultMessage: "Hi Mum, I am back home"
+            },
+            {
+                id: 2,
+                name: "Wife",
+                telNo: "07777 777 888",
+                avatarUrl: "https://cdn3.iconfinder.com/data/icons/cool-avatars-2/190/00-32-2-512.png",
+                defaultMessage: "Hi Darling, I am back home"
+            },
+            {
+                id: 3,
+                name: "Bestie",
+                telNo: "07777 777 666",
+                avatarUrl: "https://cdn3.iconfinder.com/data/icons/cool-avatars-2/190/00-38-2-512.png",
+                defaultMessage: "Hiya, I am back home"
+            }
+        ];
+        setContacts(contactsData);
+    }, []);
+
   return (
-    <View>
-      <ContactList />
-      <AddContact />
-    </View>
+    <ScrollView>
+      <AddContact contacts={contacts} setContacts={setContacts} />
+      <ContactList contacts={contacts} setContacts={setContacts} />
+    </ScrollView>
   );
 }
--- src/components/contacts/ContactList.jsx
+++ src/components/contacts/ContactList.jsx
@@ -2,41 +2,18 @@ import { useState, useEffect } from "react";
 import { StyleSheet, View } from "react-native";
 import ContactCard from "./ContactCard";
 
-const ContactList = () => {
-    const [contacts, setContacts] = useState([]);
-
-    useEffect(() => {
-        // call backend here to get contacts instead of this
-        const contactsData = [
-            {
-                id: 1,
-                name: "Mum",
-                telNo: "07777 777 777",
-                avatarUrl: "https://cdn4.iconfinder.com/data/icons/cool-avatars-2/190/00-08-512.png",
-                defaultMessage: "Hi Mum, I am back home"
-            },
-            {
-                id: 2,
-                name: "Wife",
-                telNo: "07777 777 888",
-                avatarUrl: "https://cdn3.iconfinder.com/data/icons/cool-avatars-2/190/00-32-2-512.png",
-                defaultMessage: "Hi Darling, I am back home"
-            },
-            {
-                id: 3,
-                name: "Bestie",
-                telNo: "07777 777 666",
-                avatarUrl: "https://cdn3.iconfinder.com/data/icons/cool-avatars-2/190/00-38-2-512.png",
-                defaultMessage: "Hiya, I am back home"
-            }
-        ];
-        setContacts(contactsData);
-    }, []);
+const ContactList = ({contacts, setContacts}) => {
+    const handleRemove = (id) => {
+        if(id) {
+            // need to update firebase here
+            setContacts(contacts.filter((contact) => contact.id !== id));
+        }
+    };
 
     return (
         <View style={styles.contactList}>
             {contacts.map((contact) => {
-                return <ContactCard key={contact.id} contact={contact} />
+                return <ContactCard key={contact.id} contact={contact} handleRemove={handleRemove} />
             })}
         </View>
     );
