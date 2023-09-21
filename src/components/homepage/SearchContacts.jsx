15c567d0298f2bbbf5bf7ebbf8ceb8469a84f555
Amy Baker'git commit -m adding <amybaker2001@hotmail.co.uk>
2023-09-21 22:03:49 +0100

    select multiple contacts, add styling

--- src/components/homepage/SearchContacts.jsx
+++ src/components/homepage/SearchContacts.jsx
@@ -2,8 +2,8 @@ import { getUserContacts } from "../../services/api";
 import React, { useState, useEffect, useContext } from "react";
 import { Text, View, TextInput, TouchableOpacity, FlatList } from 'react-native'
 import { UserContext } from "../../services/userContext";
-
-import styles from "../../styles/Destinations.styles";
+import { StyleSheet } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 export default function SearchContacts() {
     const { currentUser } = useContext(UserContext);
@@ -11,6 +11,7 @@ export default function SearchContacts() {
     const [loading, setLoading] = useState(false);
     const [inputValue, setInputValue] = useState("");
     const [suggestions, setSuggestions] = useState([]);
+    const [selectedContacts, setSelectedContacts] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -44,23 +45,35 @@ export default function SearchContacts() {
           setSuggestions([]);
           return;
         }
-        const filteredContacts = contacts.filter((contact) =>
-        contact.name && contact.name.toLowerCase().includes(text.toLowerCase())
-      );
+     const filteredContacts = contacts.filter((contact) =>
+      contact.name && contact.name.toLowerCase().includes(text.toLowerCase())
+    ).filter((contact) => !selectedContacts.some((c) => c.id === contact.id));
+
       setSuggestions(filteredContacts.slice(0, 3));
     };
   
     const handleSelectContact = (contact) => {
-      setInputValue(contact.name);
       // add function later?
+      setInputValue("");
       setSuggestions([]); 
   
+   
+  
+    if (!selectedContacts.some((c) => c.id === contact.id)) {
+        setSelectedContacts([...selectedContacts, contact]);
+      }
     };
+  
+    const handleRemoveContact = (contact) => {
+    
+      setSelectedContacts(selectedContacts.filter((c) => c.id !== contact.id));
+    };
+  
     return (
       <View>
         <TextInput
           style={styles.input}
-          placeholder="Choose Contact..."
+          placeholder="Choose Contacts..."
           placeholderTextColor={'grey'}
           value={inputValue}
           onChangeText={handleInputChange}
@@ -70,19 +83,61 @@ export default function SearchContacts() {
             }
           }}
         />
+        {selectedContacts.map((contact) => (
+                <TouchableOpacity
+              key={contact.id}
+              onPress={() => handleRemoveContact(contact)}
+              style={styles.selectedContact}
+            >
+              <Text style={styles.selectedContactText}>{contact.name}</Text>
+            </TouchableOpacity>
+            ))}
         {inputValue !== "" && suggestions.length > 0 && (
           <FlatList
             data={suggestions}
-            keyExtractor={(item) => item.id.toString()}
+            keyExtractor={(item) => item.id}
             renderItem={({ item }) => (
               <TouchableOpacity onPress={() => handleSelectContact(item)}>
                 <Text style={styles.suggestion}>{item.name}</Text>
               </TouchableOpacity>
             )}
-            style={styles.dropdown} 
+            style={styles.dropdown}
           />
         )}
       </View>
     );
   }
-  
+
+
+const styles = StyleSheet.create({
+  input: {
+    padding: 15,
+    fontSize: 16,
+    borderBottomWidth: 1,
+    borderColor: 'grey',
+    marginBottom: 10,
+  },
+  selectedContact: {
+    backgroundColor: '#007BFF',
+    color: 'white',
+    padding: 10,
+    borderRadius: 5,
+    margin: 5,
+    textAlign: 'center',
+  },
+  suggestion: {
+    padding: 12,
+    fontSize: 16,
+    borderBottomWidth: 1,
+    borderColor: 'lightgrey',
+    backgroundColor: 'white',
+  },
+  dropdown: {
+    maxHeight: 150,
+    borderWidth: 1,
+    borderColor: 'lightgrey',
+    borderRadius: 5,
+  },
+});
+
+
