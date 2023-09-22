e7f3535831239055c91dfc1d383e0c05c6f0b78c
EdJackson98 <135015134+EdJackson98@users.noreply.github.com>
2023-09-22 11:00:10 +0100

    geocoding using google API

--- src/utils/AddToCoord.js
+++ src/utils/AddToCoord.js
@@ -1,32 +1,31 @@
-const NodeGeocoder = require('node-geocoder');
+const axios = require('axios');
 
-const geocoder = NodeGeocoder({
-  provider: 'openstreetmap',
-});
+const googleMapsApiKey = 'AIzaSyAQfQIC-Fy3-cQhPkkgH-aIwPxAumcpScw';
 
 function addressToCoordinates(address) {
   return new Promise((resolve, reject) => {
-    geocoder.geocode(address, (error, result) => {
-      if (error) {
+    const encodedAddress = encodeURIComponent(address);
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${googleMapsApiKey}`;
+
+    axios.get(url)
+      .then((response) => {
+        if (response.data.status === 'OK' && response.data.results.length > 0) {
+          const { lat, lng } = response.data.results[0].geometry.location;
+          resolve({ latitude: lat, longitude: lng });
+        } else {
+          console.log('Geocoding failed for the given address.');
+          resolve(null);
+        }
+      })
+      .catch((error) => {
         console.error(`Error during geocoding: ${error.message}`);
         reject(error);
-      } else if (result.length > 0) {
-        const { latitude, longitude } = result[0];
-        resolve({ latitude, longitude });
-      } else {
-        console.log('Geocoding failed for the given address.');
-        resolve(null);
-      }
-    });
+      });
   });
 }
 
-module.exports = addressToCoordinates
-
-
-// .then() and .catch() chain to be edited in function call
-
-const address = 'McDonalds, Oxford Street, UK';
+// Usage example
+const address = "McDonald's Oxford Circus, Oxford Street, London, UK";
 
 addressToCoordinates(address)
   .then((coordinates) => {
