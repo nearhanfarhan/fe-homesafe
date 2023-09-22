bb269bfaf055faeaedd4ce1381397faba470c03e
EdJackson98 <135015134+EdJackson98@users.noreply.github.com>
2023-09-22 09:36:03 +0100

    utility functions for address/coord conversion

---
+++ src/utils/AddToCoord.js
@@ -0,0 +1,41 @@
+const NodeGeocoder = require('node-geocoder');
+
+const geocoder = NodeGeocoder({
+  provider: 'openstreetmap',
+});
+
+function addressToCoordinates(address) {
+  return new Promise((resolve, reject) => {
+    geocoder.geocode(address, (error, result) => {
+      if (error) {
+        console.error(`Error during geocoding: ${error.message}`);
+        reject(error);
+      } else if (result.length > 0) {
+        const { latitude, longitude } = result[0];
+        resolve({ latitude, longitude });
+      } else {
+        console.log('Geocoding failed for the given address.');
+        resolve(null);
+      }
+    });
+  });
+}
+
+module.exports = addressToCoordinates
+
+
+// .then() and .catch() chain to be edited in function call
+
+const address = 'McDonalds, Oxford Street, UK';
+
+addressToCoordinates(address)
+  .then((coordinates) => {
+    if (coordinates) {
+      console.log(`Latitude: ${coordinates.latitude}, Longitude: ${coordinates.longitude}`);
+    } else {
+      console.log('Geocoding failed for the given address.');
+    }
+  })
+  .catch((error) => {
+    console.error(`Error: ${error.message}`);
+  });
