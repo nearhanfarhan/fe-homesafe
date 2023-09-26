57bb596a7e9024aaaf9663827b5e12fba1a87b11
Deadline76 <dajc76@aol.com>
2023-09-26 15:12:19 +0100

    adds auto texting

--- src/components/homepage/draft.jsx
+++ src/components/homepage/draft.jsx
@@ -3,17 +3,16 @@ import { Alert, TouchableOpacity, Text } from 'react-native';
 import { GeofencingEventType } from 'expo-location';
 import * as Location from 'expo-location';
 import * as TaskManager from 'expo-task-manager';
+import * as SMS from 'expo-sms';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as Notifications from 'expo-notifications';
 import { UserContext } from "../../contexts/UserContext";
 import styles from "../../styles/Homepage.styles";
 import { Button } from '@rneui/base';
 import { NativeModules, PermissionsAndroid } from 'react-native';
-import { EventEmitter } from 'events';
 
 const GEOFENCING_TASK = 'GeofencingTask';
 const DirectSms = NativeModules.DirectSms;
-const trackingEvents = new EventEmitter();
 
 TaskManager.defineTask(GEOFENCING_TASK, ({ data: { eventType }, error }) => {
   if (error) {
@@ -21,12 +20,11 @@ TaskManager.defineTask(GEOFENCING_TASK, ({ data: { eventType }, error }) => {
     return;
   }
   if (eventType === GeofencingEventType.Enter) {
-    AsyncStorage.multiGet(['mobileNum', 'smsBody']).then((data) => {
+    AsyncStorage.multiGet('mobileNum', 'smsBody').then((data) => {
         const asyncMob = data[0][1];
         const asyncBody = data[1][1]
         DirectSms.sendDirectSms(asyncMob, asyncBody)
     })
-    Alert.alert('Destination reached, SMS sent')
     Notifications.scheduleNotificationAsync({
       content: {
         title: "Destination reached, SMS sent, tracking stopped!",
@@ -35,18 +33,16 @@ TaskManager.defineTask(GEOFENCING_TASK, ({ data: { eventType }, error }) => {
       trigger: null,
     });
     Location.stopGeofencingAsync(GEOFENCING_TASK);
-    trackingEvents.emit('trackingStatusChanged', true)
-    console.log('tracking stopped')
   }
 });
 
 export const TrackMyJourney = ({selectedContacts, selectedDestination}) => {
   const [isTracking, setIsTracking] = useState(false)
+
   const { currentUser } = useContext(UserContext);
   const user = currentUser?.displayName || '';
 
   useEffect (() => {
-    if (selectedContacts && selectedContacts.length > 0)
     AsyncStorage.setItem('mobileNum', selectedContacts[0].telNo)
   },[selectedContacts])
 
@@ -55,25 +51,12 @@ export const TrackMyJourney = ({selectedContacts, selectedDestination}) => {
     AsyncStorage.setItem('smsBody', smsBody)
   }, [selectedDestination])
 
-  useEffect (() => {
-    const handleTrackingChange = (newStatus) => {
-      if(newStatus) {
-        setIsTracking(false)
-      }
-    }
-    trackingEvents.on('trackingStatusChanged', handleTrackingChange)
-    
-    return () => {
-      trackingEvents.off('trackingStatusChanged', handleTrackingChange);
-    };
-  }, [isTracking])
-
 
   const smsPermission = () => {
     PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.SEND_SMS)
       .then((response) => {
         if (response === PermissionsAndroid.RESULTS.GRANTED) {
-          console.log('Notification perms')
+          console.log('perms granted')
         } else {
           console.log('perms denied')
         }
@@ -83,6 +66,7 @@ export const TrackMyJourney = ({selectedContacts, selectedDestination}) => {
   const requestNotificationPermission = async () => {
     const { status } = await Notifications.requestPermissionsAsync();
     if (status !== 'granted') {
+      console.log('Notification permission denied');
       return false;
     }
     return true;
@@ -116,7 +100,6 @@ export const TrackMyJourney = ({selectedContacts, selectedDestination}) => {
       ])
       .then(() => {
         Alert.alert('Tracking started.')
-        console.log('tracking started')
       })
     })
     .catch((error) => {
@@ -129,16 +112,11 @@ export const TrackMyJourney = ({selectedContacts, selectedDestination}) => {
   const handleStopTracking = () => {
     if (isTracking){
       setIsTracking(false)
-     Location.stopGeofencingAsync(GEOFENCING_TASK)
-     .then(() => {
-      console.log('handle-stopped')
-      Alert.alert('Tracking stopped.')  
-     })
-     .catch(err => {
-      console.log('error stopping geofencing')
-     })
+     Location.stopGeofencingAsync(GEOFENCING_TASK);
+     console.log('stopped')
+      Alert.alert('Tracking stopped.')
+    }
   }
-}
 
   return (
     <>
