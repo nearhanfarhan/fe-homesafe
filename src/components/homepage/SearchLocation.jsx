fd655756f2a37f25ea5139bc0fa518b79aff7d9e
ccdu1010 <ccdu1010@gmail.com>
2023-09-27 10:16:44 +0100

    style fix on location  search

--- src/components/homepage/SearchLocation.jsx
+++ src/components/homepage/SearchLocation.jsx
@@ -1,10 +1,9 @@
-import React, { useEffect } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import { AddressToCoordinates } from '../../utils/AddToCoord';
+import { View, StyleSheet } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { AddressToCoordinates } from "../../utils/AddToCoord";
 
 export default function SearchLocation({
-  query,
+  placeholder,
   setQuery,
   locations,
   setLocations,
@@ -13,65 +12,55 @@ export default function SearchLocation({
 }) {
   const handlePlaceSelected = (place) => {
     AddressToCoordinates(place.description).then((coords) => {
-      setSelectedDestination({ ...selectedDestination, ...coords })
+      setSelectedDestination({ ...selectedDestination, ...coords });
     });
-    // setSelectedDestination(place);
     setQuery(place.description);
     setLocations([]);
   };
 
-  // useEffect(() => {
-  //   // You can use the `locations` state to update the autocomplete predictions
-  //   // based on the user's input. You can pass it as the `predefinedPlaces` prop.
-  // console.log(selectedDestination)
-  
-  // }, [selectedDestination]);
-
   return (
     <View style={styles.searchContainer}>
-        <View style={styles.autocompleteContainer}>
-          <GooglePlacesAutocomplete
-            placeholder="Search for a location"
-            minLength={2} 
-            autoFocus={false}
-            returnKeyType={'search'}
-            listViewDisplayed="auto"
-            fetchDetails={true}
-            renderDescription={(row) => row.description}
-            onPress={(data, details = null) => {
-              handlePlaceSelected(data);
-            }}
-            onFail={(error) => console.error(error)}
-            query={{
-              key: 'AIzaSyDvVmqahHXDsFvalXZLkcfh5PL5F4Id8zo',
-              language: 'en',
-            }}
-            enablePoweredByContainer={false}
-            styles={{
-              textInputContainer: {
-                width: '100%'
-              },
-              description: {
-                fontWeight: 'bold',
-              },
-              predefinedPlacesDescription: {
-                color: '#1faadb',
-              },
-            }}
-            // Pass your predefinedPlaces (locations) here to update autocomplete predictions
-            predefinedPlaces={locations}
-          />
-        </View>
+      <View style={styles.autocompleteContainer}>
+        <GooglePlacesAutocomplete
+          placeholder={placeholder}
+          minLength={2}
+          autoFocus={false}
+          returnKeyType={"search"}
+          listViewDisplayed="auto"
+          fetchDetails={true}
+          renderDescription={(row) => row.description}
+          onPress={(data, details = null) => {
+            handlePlaceSelected(data);
+          }}
+          onFail={(error) => console.error(error)}
+          query={{
+            key: "AIzaSyDvVmqahHXDsFvalXZLkcfh5PL5F4Id8zo",
+            language: "en",
+          }}
+          enablePoweredByContainer={false}
+          styles={{
+            textInputContainer: {
+              width: "100%",
+            },
+            description: {
+              fontWeight: "bold",
+            },
+            predefinedPlacesDescription: {
+              color: "#1faadb",
+            },
+          }}
+          // Pass your predefinedPlaces (locations) here to update autocomplete predictions
+          predefinedPlaces={locations}
+        />
       </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   searchContainer: {
-    position: "relative",
-    flex: 1,
-    paddingTop: 50,
     paddingBottom: 20,
+    height: 60,
   },
   autocompleteContainer: {
     flex: 1,
@@ -79,7 +68,8 @@ const styles = StyleSheet.create({
     position: "absolute",
     right: 0,
     top: 0,
-    zIndex: 1,
+    zIndex: 2,
+    elevation: Platform.OS === "android" ? 50 : 0,
     padding: 5,
-  }
+  },
 });
