cb2d415b8da2b96859eb7babd78e5a354210860b
Ali Imran <real.imrano@proton.me>
2023-09-27 16:15:42 +0100

    added radius selector functionality within app to allow user to select radius depending on size of destination

--- src/app/home/index.jsx
+++ src/app/home/index.jsx
@@ -7,15 +7,17 @@ import MapHP from "../../components/homepage/MapHP";
 import SearchLocation from "../../components/homepage/SearchLocation";
 import { TrackMyJourney } from "../../components/homepage/TrackMyJourney";
 import SearchContacts from "../../components/homepage/SearchContacts";
+import RadiusSelector from "../../components/homepage/RadiusSelector";
 
 export default function HomePage() {
   if (!auth.currentUser) {
     return <Redirect href="/login" />;
   }
-  const [selectedDestination, setSelectedDestination] = useState({ identifier: 'home', latitude: 51.468100, longitude: -0.187800, radius: 100, });
+  const [selectedDestination, setSelectedDestination] = useState({ identifier: 'home', latitude: 51.468100, longitude: -0.187800, radius: 150, });
   const [selectedContacts, setSelectedContacts] = useState([]);
   const [query, setQuery] = useState('');
   const [locations, setLocations] = useState([]);
+  const [selectedRadius, setSelectedRadius] = useState({size: 'medium', radius: 150})
   
   return (
     <SafeAreaView style={styles.container}>
@@ -28,9 +30,9 @@ export default function HomePage() {
         locations={locations}
         setLocations={setLocations} />
       <SearchContacts setSelectedContacts={setSelectedContacts} />
-      <MapHP selectedDestination={selectedDestination} setSelectedDestination={setSelectedDestination} />
+      <RadiusSelector selectedRadius={selectedRadius} setSelectedRadius={setSelectedRadius} selectedDestination={selectedDestination} setSelectedDestination={setSelectedDestination}/>
+      <MapHP selectedDestination={selectedDestination} setSelectedDestination={setSelectedDestination} selectedRadius={selectedRadius} />
       <TrackMyJourney selectedContacts={selectedContacts} selectedDestination={selectedDestination}/>
     </SafeAreaView>
   )
 }
-      
--- src/components/homepage/MapHP.jsx
+++ src/components/homepage/MapHP.jsx
@@ -1,7 +1,7 @@
 import { Text, StyleSheet } from "react-native"
 import MapView, { Marker, Callout, Circle } from 'react-native-maps';
 
-export default function MapHP({selectedDestination, setSelectedDestination}) {
+export default function MapHP({selectedDestination, setSelectedDestination, selectedRadius}) {
     // this needs setting to the actual location
     const initialRegion = {
         latitude: 53.78825,
@@ -10,7 +10,7 @@ export default function MapHP({selectedDestination, setSelectedDestination}) {
         longitudeDelta: 0.0421,
     };
 
-    const defaultRadius = 100;
+    // const defaultRadius = 100;
 
     return (
         <MapView style={styles.map}
@@ -42,7 +42,7 @@ export default function MapHP({selectedDestination, setSelectedDestination}) {
                             latitude: selectedDestination.latitude,
                             longitude: selectedDestination.longitude,
                         }}
-                        radius={defaultRadius} // Set the radius (in meters)
+                        radius={selectedRadius.radius} // Set the radius (in meters)
                         fillColor="rgba(0, 128, 255, 0.2)" // Fill color of the circle
                         strokeColor="rgba(0, 0, 255, 0.5)" // Stroke color of the circle
                         />
@@ -55,6 +55,6 @@ export default function MapHP({selectedDestination, setSelectedDestination}) {
 const styles = StyleSheet.create({
     map: {
         width: "100%",
-        height: "65%"
+        height: "50%"
     }
 });
